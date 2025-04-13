30284d1b0db0b83dbcb5ed687a2cd205b307992d
jimsihk <99048231+jimsihk@users.noreply.github.com>
2025-04-13 23:26:14 +0800

    Add iconv test

--- rootfs/var/www/html/index.php
+++ rootfs/var/www/html/index.php
@@ -1,2 +1,14 @@
 <?php
 phpinfo();
+
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
+$text = "This is the Euro symbol '€'.";
+$result = iconv("UTF-8", "ASCII//TRANSLIT//IGNORE", $text);
+
+if ($result === false) {
+    echo "iconv failed: " . error_get_last()['message'] . "\n";
+} else {
+    echo "iconv succeeded: $result\n";
+}
