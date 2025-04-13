5261cebb2de92135d8b5308287ae81bc079226bf
jimsihk <99048231+jimsihk@users.noreply.github.com>
2025-04-13 23:36:16 +0800

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 curl --silent --fail http://app:8080 | grep 'PHP 8.3'
-curl --silent --fail http://app:8080 | grep 'iconv'
+curl --silent --fail http://app:8080 | grep 'iconv test'
