562af222b9e9236595b2c6be5dc132ffa18dab21
jimsihk <99048231+jimsihk@users.noreply.github.com>
2025-04-13 23:33:33 +0800

    Update index.php

--- rootfs/var/www/html/index.php
+++ rootfs/var/www/html/index.php
@@ -1,12 +1,12 @@
 <?php
 phpinfo();
 
-ini_set('display_errors', '1');
-error_reporting(E_ALL);
+//ini_set('display_errors', '1');
+//error_reporting(E_ALL);
 
+// Below for testing iconv issue of alpine
 $text = "This is the Euro symbol '€'.";
 $result = iconv("UTF-8", "ASCII//TRANSLIT//IGNORE", $text);
-
 if ($result === false) {
     echo "iconv failed: " . error_get_last()['message'] . "\n";
 } else {
