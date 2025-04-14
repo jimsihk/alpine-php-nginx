9d561fed2ce70ac10c582ddcc869b9b80be6fdb3
jimsihk <99048231+jimsihk@users.noreply.github.com>
2025-04-14 23:47:22 +0800

    Add test for alpine iconv issue (#272)

--- rootfs/var/www/html/index.php
+++ rootfs/var/www/html/index.php
@@ -1,2 +1,12 @@
 <?php
-phpinfo();
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
+// Below for testing iconv issue of alpine
+$text = "This is the Euro symbol '€'.";
+$result = iconv("UTF-8", "ASCII//TRANSLIT//IGNORE", $text);
+
+// Output PHP version if all test passed
+if ($result !== false) {
+    print_r(phpversion());
+}
