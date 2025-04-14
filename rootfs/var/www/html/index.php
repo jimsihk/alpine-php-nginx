425f13c81c22d355cd961e2c9cc434815b57c334
jimsihk <99048231+jimsihk@users.noreply.github.com>
2025-04-14 22:46:42 +0800

    Update index.php

--- rootfs/var/www/html/index.php
+++ rootfs/var/www/html/index.php
@@ -8,7 +8,7 @@ error_reporting(E_ALL);
 $text = "This is the Euro symbol '€'.";
 $result = iconv("UTF-8", "ASCII//TRANSLIT//IGNORE", $text);
 if ($result === false) {
-    print_r("iconv test failed: " . error_get_last()['message'] . "\n");
+    print("iconv test failed: " . error_get_last()['message'] . "\n");
 } else {
-    print_r("iconv test passed: $result\n");
+    print("iconv test passed: $result\n");
 }
